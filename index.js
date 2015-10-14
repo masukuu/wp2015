b522f9f22790d1135e8361e2df033e280354e6a8
masukuu <mask42302@gmail.com>
2015-10-15 01:52:09 +0800

    151015

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+//window bounce
+	$(window).fancy_scroll({
+	animation: "bounce"
+	});
+	
+	
+//nyan voice
+	var aud = document.getElementById("nyan_voice");
+	$("#mouseImg").hover(function(){
+		aud.play();
+		}, function(){
+		aud.pause();
+	});
+
+
+//folder animation
 	$(".folderText").hide();
+	
 	var box=$(".box");
-	//$("h2").click(function(){
-	document.getElementById('folder1').onclick = function(){ 
-	box.toggleClass("openned").toggleClass("showafter");
-	$(".folderText").toggle(400);
+	$("#folder1").click(function(){ box.toggleClass("openned").toggleClass("showafter");
+		$(".folderText").toggle(400);
 		//folderText.style.opacity = 1;
-	};
-	//}); 
-box.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){ 
-  box.toggleClass("transitionstart")
-     .toggleClass("transitionend");
-});
+	}); 
+	//document.getElementById('folder1').onclick = function(){}; 
+	
+	box.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){ 
+	box.toggleClass("transitionstart")
+    .toggleClass("transitionend");
+	});
 	var box_tedx=$(".box");
